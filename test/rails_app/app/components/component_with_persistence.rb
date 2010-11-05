5881c2019df636ad7cb29429abd3c68809c770cb
Sergei Kozlov <sergei@playcode.nl>
2010-11-05 20:24:08 +0100

    Clean-up, documenting

--- test/rails_app/app/components/component_with_persistence.rb
+++ test/rails_app/app/components/component_with_persistence.rb
@@ -1,9 +1,12 @@
 class ComponentWithPersistence < Netzke::Base
-  config :initial, :persistence => true, :title => "Default title"
-  
+  config :default, {
+    :persistence => true,
+    :title => "Default title"
+  }
+
   action :change_title
   action :set_original_title
-  
+
   js_property :bbar, [:change_title.action, :set_original_title.action]
 
   js_method :on_change_title, <<-JS
@@ -11,21 +14,20 @@ class ComponentWithPersistence < Netzke::Base
       this.setNewTitle();
     }
   JS
-  
+
   js_method :on_set_original_title, <<-JS
     function(){
       this.setOriginalTitle();
     }
   JS
-  
+
   endpoint :set_new_title do |params|
-    update_persistent_options(:title => "New title")
+    update_persistent_options(:title => "New title") # this way :title will get merged into config
     {}
   end
-  
+
   endpoint :set_original_title do |params|
-    update_persistent_options(:title => nil)
-    Rails.logger.debug "!!! persistent_options: #{persistent_options.inspect}\n"
+    update_persistent_options(:title => nil) # setting to nil means removing the key, so the default value be used
     {}
   end
 end
