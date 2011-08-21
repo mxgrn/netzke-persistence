38772a700f87f17d8f432f012806b06d3bcab8cc
Sergei Kozlov <sergei@playcode.nl>
2011-08-21 18:50:19 +0200

    Renamed rails_app to persistence_test_app

--- test/persistence_test_app/config/routes.rb
+++ test/persistence_test_app/config/routes.rb
@@ -2,11 +2,11 @@ RailsApp::Application.routes.draw do
   get "welcome/index"
 
   devise_for :users
-  
+
   get "sign_in", :to => "devise/sessions#new"
 
   netzke
-  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -63,6 +63,6 @@ RailsApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-  
+
   match 'components/:component' => 'components#index', :as => "components"
 end
